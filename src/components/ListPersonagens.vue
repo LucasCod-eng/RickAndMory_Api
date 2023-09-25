9f7170ce5f18ee85dbafd81cf79db8281411d62f
Lucas Gabriel <lucgabriel002@gmail.com>
2023-09-24 21:44:24 -0300

    Primeiro commit

---
+++ src/components/ListPersonagens.vue
@@ -0,0 +1,17 @@
+<script setup>
+const personagem = defineProps(["name"])
+</script>
+
+<template>
+    <div class="col-4">
+        <div class="card mb-3">
+            <p class="texte-center">{{ personagem.name }}</p>
+            <img src=" " class="card-img-top" height="80">   
+        </div>
+    </div>
+
+</template>
+
+<style>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { onMounted, reactive, ref } from 'vue';
+import { RouterLink } from 'vue-router';
+
+let character = reactive(ref());
+
+onMounted(() => {
+	fetch("https://rickandmortyapi.com/api/character").then(res => { res.json().then((x) => { character = x.results; console.log(character); }) })
+})
+
+</script>
+
+<template>
+	<div class="container texte-center">
+		<div class="row">
+			<div class="row col-12">
+				<div v-for="c in character" class="col-6 d-flex justify-content-center">
+					<div class="col-sm-12 col-md-6">
+						<div class="card">
+							<div class="card-body row">
+								<div class=" "><strong>{{ c.name }}</strong></div>
+								<img :src="c.image">
+								<div>
+									<p class="mt-4"> <strong>Espécie:</strong> {{ c.species }}</p>
+									<p class="mt-4"> <strong>Gênero:</strong> {{ c.gender }}</p>
+									<p class="mt-4"><strong>Status:</strong> {{ c.status }}</p>
+
+								</div>
+								<RouterLink to="/">
+									<button class="btn btn-dark">Info Skills</button>
+								</RouterLink>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
